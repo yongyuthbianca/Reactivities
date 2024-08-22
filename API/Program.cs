086b2c70fbd067a78027992d8174bf22f662350e
Bianca Yongyuth <63754318+yongyuthbianca@users.noreply.github.com>
2024-08-22 16:40:49 -0700

    End of section 3

--- API/Program.cs
+++ API/Program.cs
@@ -14,6 +14,12 @@ builder.Services.AddDbContext<DataContext>(opt =>
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 }
 );
+builder.Services.AddCors(opt =>{
+    opt.AddPolicy("CorsPolicy", policy =>
+    {
+        policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+    });
+});
 
 var app = builder.Build();
 
@@ -24,6 +30,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseCors("CorsPolicy");
 app.UseAuthorization();
 
 app.MapControllers();
